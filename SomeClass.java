ea2d36d361ba7e72d39d65013a8147195157217d
MrCoder <eagle.xiao@gmail.com>
2023-03-28 19:39:17 +1100

    Removed feature bread

--- SomeClass.java
+++ SomeClass.java
@@ -3,8 +3,7 @@ public class Person {
     private int age;
 
     public Person(String name, int age) {
-        // feature bread sets name to upper case
-        this.name = name.toUpperCase();
+        this.name = name;
         // feature cheese doubles the age
         this.age = age * 2;
     }
